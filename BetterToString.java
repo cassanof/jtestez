5609ee64a175b4fa9b6d6cf1ba10aa6d9f5c383b
Justin Liu <93306309+liujustin53@users.noreply.github.com>
2022-05-07 15:58:00 -0400

    toString

--- BetterToString.java
+++ BetterToString.java
@@ -3,8 +3,38 @@
 class BetterToString {
   public static <T> String make(T obj) {
     if (obj instanceof String) {
-      // TODO: actually do the conversion
-      return obj.toString();
+//      // TODO: actually do the conversion
+//      return "\"" + obj.toString() + "\"";
+      String buffer = "";
+      char[] chars = ((String) obj).toCharArray();
+      for (char c : chars) {
+        switch(c) {
+          case '\t':
+            buffer += "\\" + "t";
+            break;
+          case '\b':
+            buffer += "\\" + "b";
+            break;
+          case '\n':
+            buffer += "\\" + "n";
+            break;
+          case '\r':
+            buffer += "\\" + "r";
+            break;
+          case '\f':
+            buffer += "\\" + "f";
+            break;
+          case '\'':
+          case '\"':
+          case '\\':
+            buffer += "\\" + c;
+            break;
+          default:
+            buffer += c;
+            break;
+        }
+      }
+      return "\"" + buffer + "\"";
     }
     else {
       return obj.toString();
